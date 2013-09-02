368809d662873ca80d4f964f92b44b879cc836d3
Fernando Álvarez <alvarez.calleja.fernando@gmail.com>
2013-09-02 13:00:44 +0200

    Match view done

--- app.rb
+++ app.rb
@@ -9,8 +9,17 @@ class Player < ActiveRecord::Base
 end
 
 class Game < ActiveRecord::Base
-	belongs_to :team1backend, :class_name => 'User', :foreign_key => 'team1backend_id'
-	belongs_to :team1frontend, :class_name => 'User', :foreign_key => 'team1frontend_id'
-	belongs_to :team2backend, :class_name => 'User', :foreign_key => 'team2backend_id'
-	belongs_to :team2frontend, :class_name => 'User', :foreign_key => 'team2frontend_id'
+	belongs_to :team1backend, :class_name => 'Player', :foreign_key => 'team1backend_id'
+	belongs_to :team1frontend, :class_name => 'Player', :foreign_key => 'team1frontend_id'
+	belongs_to :team2backend, :class_name => 'Player', :foreign_key => 'team2backend_id'
+	belongs_to :team2frontend, :class_name => 'Player', :foreign_key => 'team2frontend_id'
+end
+
+get "/" do
+	erb :match
+end
+
+post "/result" do
+	@params = params[:post]
+	erb :result
 end
